854328250bd88eb550eb16b031a53d90c973f948
Nico Knoll <mail@nico.is>
2015-09-06 15:08:35 +0200

    Fixed issues. Import should work now.

--- blnfmcalsync.php
+++ blnfmcalsync.php
@@ -3,7 +3,7 @@
 Plugin Name: BLN.FM Cal Sync
 Description: 
 Author: Nico Knoll
-Version: 1.6
+Version: 1.7
 Author URI: http://nico.is
 */
 
@@ -31,12 +31,24 @@ function spreadsheetToArray() {
 	$data = json_decode(file_get_contents_curl($url), true);
 	$return = array();
 
+	if(!count($data['feed']['entry'])) return false;
+
 	foreach($data['feed']['entry'] as $event) {
 		$return_event = array();
 		foreach($event as $key => $value) {
 			if(preg_match("%^gsx\\$%Uis", $key)) {
 				$key = str_replace('gsx$', '', $key);
-				$return_event[$key] = $value["\$t"];
+				if($key == '_cn6ca') $key = 'starttag';
+
+				if($key == 'starttag' || $key == 'endtag') {
+					$tmp = explode('.', $value["\$t"]);
+					$return_event[$key] = date('Y-m-d', mktime(0,0,0,$tmp[1],$tmp[0],$tmp[2]));
+				} elseif($key == 'startzeit' || $key == 'endzeit') {
+					$tmp = explode(':', $value["\$t"]);
+					$return_event[$key] = date('H:i:s', mktime($tmp[0],$tmp[1],0,0,0,0));
+				} else {
+					$return_event[$key] = $value["\$t"];
+				}
 			}
 		}
 		$return[] = $return_event;
@@ -45,12 +57,27 @@ function spreadsheetToArray() {
 	return $return;
 }
 
+function addLocation($name) {
+	$em_location = new EM_Location();
+
+	$em_location->location_name = $name;
+	$em_location->location_address = $name;
+	$em_location->location_town = 'Berlin';
+	$em_location->location_state = 'Berlin';
+	$em_location->location_country = 'DE';
+
+	$em_location->save();
+	$em_location->save_meta();
+
+	return $em_location->location_id;
+}
+
 function getLocationIDs() {
 	$EM_Locations = EM_Locations::get();
 	$return = array();
 
 	foreach($EM_Locations as $EM_Location){
-	  	$return[$EM_Location->name] = $EM_Location->id; 
+	  	@$return[$EM_Location->name] = @$EM_Location->id; 
 	}
 
 	return $return;
@@ -60,41 +87,56 @@ function getLocationIDs() {
 function addEvent($data) {
 	$em_event = new EM_Event();
 
-	$em_event->event_start_date = $data["endtag"];
+	$em_event->event_start_date = $data["starttag"];
 	$em_event->event_start_time = $data["startzeit"];
 	$em_event->event_end_date = $data["endtag"];
 	$em_event->event_end_time = $data["endzeit"];
+
+	$em_event->start = strtotime($em_event->event_start_date." ".$em_event->event_start_time);
+	$em_event->end = strtotime($em_event->event_end_date." ".$em_event->event_end_time);
 
 	$em_event->location_id = $data["locationid"];
 
 	$em_event->post_title = $data["titel"];
 	$em_event->event_name = $data["titel"];
 
-	$em_event->post_content = $data["kurzbeschreibung"];
+	$em_event->body = (($data["kurzbeschreibung"]) ? $data["kurzbeschreibung"] : '');
+	$em_event->post_content = (($data["kurzbeschreibung"]) ? $data["kurzbeschreibung"] : '');
 	$em_event->post_tags = $data[""];
 
-	return $em_event->save();
+	// meta
+	$em_event->group_id = 0;
+	$em_event->event_date_modified = date('Y-m-d H:i:s', time());
+	$em_event->event_all_day = 0;
+	$em_event->event_rsvp = 0;
+
+	$check = $em_event->save();
+
+	wp_update_post(array('ID' => $em_event->post_id));
+
+	var_dump($em_event->post_id);
+
+	return $check;
 }
 
 function addEvents() {
-	$locations = getLocationIDs();
+	$spreadsheet = spreadsheetToArray();
+	if($spreadsheet)
+		foreach(spreadsheetToArray() as $event) {
+			// reload locations as we dynamically create them if missing
+			$locations = getLocationIDs();
 
-	foreach(spreadsheetToArray() as $event) {
-		$event['locationid'] = $locations[$event['venue']];
-		if($event['locationid'] == '') $event['venue'];
+			$event['locationid'] = @$locations[$event['venue']];
+			if($event['locationid'] == '') $event['locationid'] = addLocation($event['venue']);
 
-		addEvent($event);
+			addEvent($event);
+		}
 	}
 }
 
-function blnfmcalsync() {
-	$chosen = hello_dolly_get_lyric();
-	echo "<p id='dolly'>$chosen</p>";
-}
 
 function blnfmcalsync_page_function() {
-
-	if($_POST['syncnow']) {
+	if(@$_POST['syncnow']) {
 		echo '<div id="message" class="updated">
 		<p>Veranstaltungen sollten jetzt synchronisiert sein.</p>
 		</div>';
@@ -116,9 +158,6 @@ function blnfmcalsync_page_function() {
 	<input type="submit" id="dbem_options_submit" class="button-primary" name="syncnow" value="Jetzt synchronisieren">
 	</form>';
 }
-
-//add_action( 'admin_notices', 'blnfmcalsync' );
-
 
 
 
